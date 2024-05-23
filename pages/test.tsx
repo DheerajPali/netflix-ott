e5f45220ec964e047eccf27c8f92e2214b112165
Dheeraj Pali <dheerajpali761@gmail.com>
2024-05-23 14:41:31 +0530

    initial Commit

---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+import "../app/globals.css";
+const MyPage = () => {
+    return(
+        <>
+        <h1 className="text-2xl text-green-500">Hello New Page</h1>
+        </>
+        
+    )
+}
+export default MyPage;
