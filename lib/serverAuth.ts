ce2ade7f677ef67e201363505f41a603f6c7f520
Dheeraj Pali <dheerajpali761@gmail.com>
2024-05-30 18:57:35 +0530

    Added Favourite List functionality but facing error due to session is getting null, will fix it soon.

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest } from "next";
 import { getSession } from "next-auth/react";
-
 import prismadb from "@/lib/prismadb";
 
 const serverAuth = async (req: NextApiRequest) => {
   const session = await getSession({ req });
+  console.log("Session:",session);
 
-  if (!session?.user?.email) {
-    throw new Error("Not signed in");
+console.log("session.user.email",session?.user?.email)
+  if (!session || !session.user || !session.user.email) {
+    console.log('Session or user email not found');
+    throw new Error("No active session found or user email is missing");
   }
 
   const currentUser = await prismadb.user.findUnique({
@@ -16,11 +18,11 @@ const serverAuth = async (req: NextApiRequest) => {
     },
   });
 
-  if(!currentUser){
-    throw new Error('Not signed in');
+  if (!currentUser) {
+    throw new Error('User not signed in or does not exist');
   }
 
-  return {currentUser}
+  return { currentUser };
 };
 
 export default serverAuth;
